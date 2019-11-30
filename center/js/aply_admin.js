ab4d919cf2170071bc8239af8327c516335c8ea9
Gunwoo Lee <gunman@aply.biz>
2019-11-30 23:17:57 +0900

    for test

--- center/js/aply_admin.js
+++ center/js/aply_admin.js
@@ -1,26 +1,26 @@
 
 var currentNetworkData;
-function getUserNetwork() {    
+function getUserNetwork() {
     var token = getCookie("user_token");
-    var emailid = getCookie("dev_user_id");    
+    var emailid = getCookie("dev_user_id");
 
     var jdata = {
       "daction": "admin_user_network",
       "emailid" : emailid,
-      "token" : token,      
+      "token" : token,
       "action" : "developer"};
 
     ajaxRequest(jdata, function (r) {
       if(r.result == "success") {
-        if (r.data.length > 0) {       
-        	currentNetworkData = r.data;          
+        if (r.data.length > 0) {
+        	currentNetworkData = r.data;
         	hideLoader();
-        	onlyConnectedNetworkList();          
+        	onlyConnectedNetworkList();
         }
-        else {          
+        else {
           hideLoader();
         }
-      }else {        
+      }else {
         hideLoader();
       }
     }, function(request, status, error) {
@@ -30,13 +30,13 @@ function getUserNetwork() {
 }
 
 
-function onlyConnectedNetworkList() 
-{		
+function onlyConnectedNetworkList()
+{
 		showLoader();
 	  var g = new Dracula.Graph();
-	  
+
 	  var edata = currentNetworkData;
-	  
+
 	  if (edata == null) {
 	  	alert("Empty");
 	  	hideLoader();
@@ -44,30 +44,34 @@ function onlyConnectedNetworkList()
 	  }
 
 		edata.forEach(function(element){
-			
 				if (  (element[0] != "-" && element[0] != null && element[0] != "null")
 							&& (element[1] != "-" && element[1] != null && element[1] != "null")
 							&& (element[0] != element[1])  )
-    		g.addEdge(element[0], element[1]);
-    	
-		});		
-		
+              {
+            		g.addEdge(element[0], element[1]);
+
+                $("#" + element[0]).live('click',function(){
+                  alert("hehe" + element[0]);
+                });
+              }
+		});
+
 		var layouter = new Dracula.Layout.Spring(g);
 		layouter.layout();
-		
+
 		var renderer = new Dracula.Renderer.Raphael('#usernetwork', g, $('#usernetwork').width(), 1024);
 		renderer.draw();
-		
+
 		hideLoader();
 }
 
-function allNetworkList() 
+function allNetworkList()
 {
 		showLoader();
 	  var g = new Dracula.Graph();
-	  
+
 	  var edata = currentNetworkData;
-	  
+
 	  if (edata == null) {
 	  	alert("Empty");
 	  	hideLoader();
@@ -81,19 +85,82 @@ function allNetworkList()
 			else {
     		g.addEdge(element[0], element[1]);
     	}
-		});		
-		
+		});
+
 		var layouter = new Dracula.Layout.Spring(g);
 		layouter.layout();
-		
+
 		var renderer = new Dracula.Renderer.Raphael('#usernetwork', g, $('#usernetwork').width(), 1024);
 		renderer.draw();
-		
+
 		hideLoader();
 }
 
+function setMapbox() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VubWFuOTciLCJhIjoiY2lvOWFpdDc5MDMxdnZpbHpocjRmMzI2ZyJ9.lnenicwIbPNDr2k7gbKLbA';
+      var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+    // San Francisco
+var origin = [-122.414, 37.776];
+
+// Washington DC
+var destination = [-77.032, 38.913];
+
+// A simple line from origin to destination.
+var route = {
+      "type": "FeatureCollection",
+      "features": [{
+          "type": "Feature",
+          "geometry": {
+              "type": "LineString",
+              "coordinates": [
+                origin,
+                destination
+              ]
+            }
+      }]
+};
+
+// A single point that animates along the route.
+// Coordinates are initially set to origin.
+var point = {
+    "type": "FeatureCollection",
+    "features": [{
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": origin
+      }
+  }]
+};
+
+  map.on('load', function () {
+      map.addSource('route', {
+          "type": "geojson",
+          "data": route
+      });
+
+      map.addLayer({
+          "id": "route",
+          "source": "route",
+          "type": "line",
+          "paint": {
+            "line-width": 2,
+            "line-color": "#007cbf"
+          }
+      });
+  });
+}
+
 function initAdmin() {
-	getUserNetwork();  
+  setMapbox();
+  hideLoader();
+
+	//getUserNetwork();
 }
 
 
